47f24a185a26ab75ec9e144366834fdc7e264b53
Simran <btech10760.21@bitmesra.ac.in>
2025-06-09 18:19:08 +0530

    Its working in the expo go app now !

--- components/loader/loader.tsx
+++ components/loader/loader.tsx
@@ -8,13 +8,13 @@ export default function Loader() {
       colors={["#E5ECF9", "#F6F7F9"]}
       style={{ flex: 1, justifyContent: "center", alignItems: "center" }}
     >
-      <AnimatedLoader
+      {/* <AnimatedLoader
         visible={true}
         overlayColor="rgba(255,255,255,0.75)"
         source={require("@/assets/animation/Online data Manager.json")}
         animationStyle={{ width: 250, height: 250 }}
         speed={1.5}
-      />
+      /> */}
     </LinearGradient>
   );
 }
